Nekogram/Nekogram

bot token login

TMessagesProj/src/main/java/org/telegram/ui/LoginActivity.java
@@ -18,6 +18,7 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
@@ -51,6 +52,7 @@ import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
@@ -207,11 +209,112 @@ public class LoginActivity extends BaseFragment {
                     if (onBackPressed()) {
                         finishFragment();
                     }
+                } else if (id == 2) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setTitle(LocaleController.getString("BotLogin", R.string.BotLogin));
+
+                    final EditTextBoldCursor editText = new EditTextBoldCursor(context) {
+                        @Override
+                        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+                            super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(AndroidUtilities.dp(64), MeasureSpec.EXACTLY));
+                        }
+                    };
+                    editText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18);
+                    editText.setTextColor(Theme.getColor(Theme.key_dialogTextBlack));
+                    editText.setHintText("Token");
+                    editText.setHeaderHintColor(Theme.getColor(Theme.key_windowBackgroundWhiteBlueHeader));
+                    editText.setSingleLine(true);
+                    editText.setFocusable(true);
+                    editText.setTransformHintToHeader(true);
+                    editText.setLineColors(Theme.getColor(Theme.key_windowBackgroundWhiteInputField), Theme.getColor(Theme.key_windowBackgroundWhiteInputFieldActivated), Theme.getColor(Theme.key_windowBackgroundWhiteRedText3));
+                    editText.setImeOptions(EditorInfo.IME_ACTION_DONE);
+                    editText.setBackgroundDrawable(null);
+                    editText.requestFocus();
+                    editText.setPadding(0, 0, 0, 0);
+                    builder.setView(editText);
+
+
+                    builder.setPositiveButton(LocaleController.getString("OK", R.string.OK), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            if (getParentActivity() == null) {
+                                return;
+                            }
+                            String token = editText.getText().toString();
+
+                            if (token.length() == 0) {
+                                needShowAlert(LocaleController.getString("AppName", R.string.AppName), LocaleController.getString("InvalidAccessToken", R.string.InvalidAccessToken));
+                                return;
+                            }
+
+                            ConnectionsManager.getInstance(currentAccount).cleanup(false);
+                            final TLRPC.TL_auth_importBotAuthorization req = new TLRPC.TL_auth_importBotAuthorization ();
+
+                            req.api_hash = BuildVars.APP_HASH;
+                            req.api_id = BuildVars.APP_ID;
+                            req.bot_auth_token = token;
+                            req.flags = 0;
+                            int reqId = ConnectionsManager.getInstance(currentAccount).sendRequest(req, (response, error) -> AndroidUtilities.runOnUIThread(() -> {
+                                if (error == null) {
+                                    TLRPC.TL_auth_authorization res = (TLRPC.TL_auth_authorization) response;
+                                    ConnectionsManager.getInstance(currentAccount).setUserId(res.user.id);
+                                    UserConfig.getInstance(currentAccount).clearConfig();
+                                    MessagesController.getInstance(currentAccount).cleanup();
+                                    UserConfig.getInstance(currentAccount).isBot = true;
+                                    UserConfig.getInstance(currentAccount).syncContacts = syncContacts;
+                                    UserConfig.getInstance(currentAccount).setCurrentUser(res.user);
+                                    UserConfig.getInstance(currentAccount).saveConfig(true);
+                                    MessagesStorage.getInstance(currentAccount).cleanup(true);
+                                    ArrayList<TLRPC.User> users = new ArrayList<>();
+                                    users.add(res.user);
+                                    MessagesStorage.getInstance(currentAccount).putUsersAndChats(users, null, true, true);
+                                    MessagesController.getInstance(currentAccount).putUser(res.user, false);
+                                    ContactsController.getInstance(currentAccount).checkAppAccount();
+                                    MessagesController.getInstance(currentAccount).getBlockedUsers(true);
+                                    MessagesController.getInstance(currentAccount).checkProxyInfo(true);
+                                    ConnectionsManager.getInstance(currentAccount).updateDcSettings();
+                                    needFinishActivity();
+                                } else {
+                                    if (error.text != null) {
+                                        if (error.text.contains("ACCESS_TOKEN_INVALID")) {
+                                            needShowAlert(LocaleController.getString("AppName", R.string.AppName), LocaleController.getString("InvalidAccessToken", R.string.InvalidAccessToken));
+                                        } else if (error.text.startsWith("FLOOD_WAIT")) {
+                                            needShowAlert(LocaleController.getString("AppName", R.string.AppName), LocaleController.getString("FloodWait", R.string.FloodWait));
+                                        } else if (error.code != -1000) {
+                                            needShowAlert(LocaleController.getString("AppName", R.string.AppName), error.text);
+                                        }
+                                    }
+                                }
+                                needHideProgress(false);
+                            }), ConnectionsManager.RequestFlagFailOnServerErrors | ConnectionsManager.RequestFlagWithoutLogin | ConnectionsManager.RequestFlagTryDifferentDc | ConnectionsManager.RequestFlagEnableUnauthorized);
+                            needShowProgress(reqId);
+                        }
+                    });
+                    builder.setNegativeButton(LocaleController.getString("Cancel", R.string.Cancel), null);
+                    builder.show().setOnShowListener(new DialogInterface.OnShowListener() {
+                        @Override
+                        public void onShow(DialogInterface dialog) {
+                            editText.requestFocus();
+                            AndroidUtilities.showKeyboard(editText);
+                        }
+                    });
+                    if (editText != null) {
+                        ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) editText.getLayoutParams();
+                        if (layoutParams != null) {
+                            if (layoutParams instanceof FrameLayout.LayoutParams) {
+                                ((FrameLayout.LayoutParams) layoutParams).gravity = Gravity.CENTER_HORIZONTAL;
+                            }
+                            layoutParams.rightMargin = layoutParams.leftMargin = AndroidUtilities.dp(24);
+                            layoutParams.height = AndroidUtilities.dp(36);
+                            editText.setLayoutParams(layoutParams);
+                        }
+                    }
                 }
             }
         });
 
         ActionBarMenu menu = actionBar.createMenu();
+        menu.addItem(2, R.drawable.list_bot);
         actionBar.setAllowOverlayTitle(true);
         doneItem = menu.addItemWithWidth(done_button, R.drawable.ic_done, AndroidUtilities.dp(56));
         doneProgressView = new ContextProgressView(context, 1);

firexgram/firexgram

Signed-off-by: nift4 <nift4@protonmail.com>
Change-Id: I2e9ef02262017aa6415237936a48c6a49d45c639
Conflicts:
	TMessagesProj/src/main/java/org/telegram/messenger/DataQuery.java
	TMessagesProj/src/main/java/org/telegram/messenger/UserConfig.java
	TMessagesProj/src/main/java/org/telegram/tgnet/TLRPC.java
	TMessagesProj/src/main/java/org/telegram/ui/LoginActivity.java

TMessagesProj/src/main/java/org/telegram/ui/LoginActivity.java
@@ -18,6 +18,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
@@ -57,6 +58,7 @@ import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewOutlineProvider;
+import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.view.inputmethod.EditorInfo;
@@ -273,6 +275,101 @@ public class LoginActivity extends BaseFragment {
                     if (onBackPressed()) {
                         finishFragment();
                     }
+                } else if (id == 2) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setTitle(LocaleController.getString("CX_BotLogin", R.string.CX_BotLogin));
+
+                    final EditTextBoldCursor editText = new EditTextBoldCursor(context) {
+                        @Override
+                        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+                            super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(AndroidUtilities.dp(64), MeasureSpec.EXACTLY));
+                        }
+                    };
+                    editText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18);
+                    editText.setTextColor(Theme.getColor(Theme.key_dialogTextBlack));
+                    editText.setHintText("Token");
+                    editText.setHeaderHintColor(Theme.getColor(Theme.key_windowBackgroundWhiteBlueHeader));
+                    editText.setSingleLine(true);
+                    editText.setFocusable(true);
+                    editText.setTransformHintToHeader(true);
+                    editText.setLineColors(Theme.getColor(Theme.key_windowBackgroundWhiteInputField), Theme.getColor(Theme.key_windowBackgroundWhiteInputFieldActivated), Theme.getColor(Theme.key_windowBackgroundWhiteRedText3));
+                    editText.setImeOptions(EditorInfo.IME_ACTION_DONE);
+                    editText.setBackgroundDrawable(null);
+                    editText.requestFocus();
+                    editText.setPadding(0, 0, 0, 0);
+                    builder.setView(editText);
+
+
+                    builder.setPositiveButton(LocaleController.getString("OK", R.string.OK), (dialogInterface, i) -> {
+                        if (getParentActivity() == null) {
+                            return;
+                        }
+                        String token = editText.getText().toString();
+
+                        if (token.length() == 0) {
+                            needShowAlert(LocaleController.getString("catox_AppName", R.string.catox_AppName), LocaleController.getString("CX_InvalidAccessToken", R.string.CX_InvalidAccessToken));
+                            return;
+                        }
+
+                        ConnectionsManager.getInstance(currentAccount).cleanup(false);
+                        final TLRPC.TL_auth_importBotAuthorization req = new TLRPC.TL_auth_importBotAuthorization ();
+
+                        req.api_hash = BuildVars.APP_HASH;
+                        req.api_id = BuildVars.APP_ID;
+                        req.bot_auth_token = token;
+                        req.flags = 0;
+                        int reqId = ConnectionsManager.getInstance(currentAccount).sendRequest(req, (response, error) -> AndroidUtilities.runOnUIThread(() -> {
+                            if (error == null) {
+                                TLRPC.TL_auth_authorization res = (TLRPC.TL_auth_authorization) response;
+                                ConnectionsManager.getInstance(currentAccount).setUserId(res.user.id);
+                                UserConfig.getInstance(currentAccount).clearConfig();
+                                MessagesController.getInstance(currentAccount).cleanup();
+                                UserConfig.getInstance(currentAccount).syncContacts = syncContacts;
+                                UserConfig.getInstance(currentAccount).setCurrentUser(res.user);
+                                UserConfig.getInstance(currentAccount).saveConfig(true);
+                                MessagesStorage.getInstance(currentAccount).cleanup(true);
+                                ArrayList<TLRPC.User> users = new ArrayList<>();
+                                users.add(res.user);
+                                MessagesStorage.getInstance(currentAccount).putUsersAndChats(users, null, true, true);
+                                MessagesController.getInstance(currentAccount).putUser(res.user, false);
+                                ContactsController.getInstance(currentAccount).checkAppAccount();
+                                MessagesController.getInstance(currentAccount).getBlockedPeers(true);
+                                ConnectionsManager.getInstance(currentAccount).updateDcSettings();
+                                needFinishActivity(false, false, 0);
+                            } else {
+                                if (error.text != null) {
+                                    if (error.text.contains("ACCESS_TOKEN_INVALID")) {
+                                        needShowAlert(LocaleController.getString("catox_AppName", R.string.catox_AppName), LocaleController.getString("CX_InvalidAccessToken", R.string.CX_InvalidAccessToken));
+                                    } else if (error.text.startsWith("FLOOD_WAIT")) {
+                                        needShowAlert(LocaleController.getString("catox_AppName", R.string.catox_AppName), LocaleController.getString("FloodWait", R.string.FloodWait));
+                                    } else if (error.code != -1000) {
+                                        needShowAlert(LocaleController.getString("catox_AppName", R.string.catox_AppName), error.text);
+                                    }
+                                }
+                            }
+                            needHideProgress(false);
+                        }), ConnectionsManager.RequestFlagFailOnServerErrors | ConnectionsManager.RequestFlagWithoutLogin | ConnectionsManager.RequestFlagTryDifferentDc | ConnectionsManager.RequestFlagEnableUnauthorized);
+                        needShowProgress(reqId);
+                    });
+                    builder.setNegativeButton(LocaleController.getString("Cancel", R.string.Cancel), null);
+                    builder.show().setOnShowListener(new DialogInterface.OnShowListener() {
+                        @Override
+                        public void onShow(DialogInterface dialog) {
+                            editText.requestFocus();
+                            AndroidUtilities.showKeyboard(editText);
+                        }
+                    });
+                    if (editText != null) {
+                        ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) editText.getLayoutParams();
+                        if (layoutParams != null) {
+                            if (layoutParams instanceof FrameLayout.LayoutParams) {
+                                ((FrameLayout.LayoutParams) layoutParams).gravity = Gravity.CENTER_HORIZONTAL;
+                            }
+                            layoutParams.rightMargin = layoutParams.leftMargin = AndroidUtilities.dp(24);
+                            layoutParams.height = AndroidUtilities.dp(36);
+                            editText.setLayoutParams(layoutParams);
+                        }
+                    }
                 }
             }
         });
@@ -282,6 +379,7 @@ public class LoginActivity extends BaseFragment {
         doneButtonVisible[DONE_TYPE_ACTION] = false;
 
         ActionBarMenu menu = actionBar.createMenu();
+        menu.addItem(2, R.drawable.list_bot);
         actionBar.setAllowOverlayTitle(true);
         doneItem = menu.addItemWithWidth(done_button, R.drawable.ic_done, AndroidUtilities.dp(56));
         doneProgressView = new ContextProgressView(context, 1);