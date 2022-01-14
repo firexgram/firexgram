601a0ce829a68c3f37b3687117ce740dedcd3828
nift4 <nift4@protonmail.com>
2022-01-14 21:17:59 +0100

    4.1.1
    fix double bottom
    fix a11+ non-media upload
    merged 8.4.3
    
    Signed-off-by: nift4 <nift4@protonmail.com>
    Change-Id: I5560024c38674647f3356299d392e1b7411a8086

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -17,7 +17,7 @@ public class BuildVars {
     public static boolean DEBUG_VERSION = false;
     public static boolean LOGS_ENABLED = false;
     public static boolean DEBUG_PRIVATE_VERSION = false;
-    public static boolean USE_CLOUD_STRINGS = false;
+    public static boolean USE_CLOUD_STRINGS = true;
     public static boolean CHECK_UPDATES = false;
     public static boolean NO_SCOPED_STORAGE = true /*Build.VERSION.SDK_INT <= 29*/;
     public static int BUILD_VERSION = 2531;
